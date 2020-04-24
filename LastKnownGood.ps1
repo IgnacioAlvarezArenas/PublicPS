077af526c768fab4339160eab8979cd65016e08c
Ignacio Alvarez Arenas <igalvare@microsoft.com>
2020-04-24 15:44:23 +0100

    2020/04/24 Adding Lastknowngood.ps1

---
+++ LastKnownGood.ps1
@@ -0,0 +1,28 @@
+﻿$partition=get-disk -Number 2 | get-partition
+$partition=$partition | ? {$_.partitionnumber -eq 2}
+$path=$partition.driveletter+":\Windows\System32\config\SYSTEM"
+reg load HKEY_USERS\ProblemSystem $path
+$current=Get-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name current
+$default=Get-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name default
+$failed=Get-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name failed
+$lastknowngood=Get-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name lastknowngood
+
+$newcurrent=$current.current+1
+$newdefault=$default.default+1
+$newfailed=$failed.failed+1
+$newlastknowngood=$lastknowngood.lastknowngood+1
+
+write-host "Setting Current Value. Old value was "$current.current 
+Set-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name current  -Value $newcurrent
+
+write-host "Setting Default Value. Old value was "$default.default
+Set-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name default  -Value $newdefault
+
+write-host "Setting Failed Value. Old value was "$failed.failed
+Set-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name failed  -Value $newfailed
+
+write-host "Setting LastKnownGood Value. Old value was "$lastknowngood.lastknowngood
+Set-ItemProperty -Path "Registry::\HKEY_USERS\Problemsystem\select" -name lastknowngood -Value $newlastknowngood
+
+
+reg unload HKEY_USERS\ProblemSystem
